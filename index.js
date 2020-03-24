54b6580a8fd2950bb4dc188f89efe6d39f12669a
lebogang-cloud <you@example.com>
2020-03-24 14:53:06 +0200

    3rd version

--- index.js
+++ index.js
@@ -1,19 +1,74 @@
-const {Visitors} = require('pg'); // Connect to a database.
-//  create a visitor object, which will connect to the database
-let visitor = new Visitors ({   
+const {Client} = require('pg')
+const client = new Client({
   user: "user",
-  host: "postgres",
-  database: "db",
   password: "pass",
-  port: 5432
-
+  host: "localhost",
+  port: "5432",
+  database: "db"
 })
 
-visitor.connect()  //returns promise
-.then(() => console.log("Connected successfully")) //tells you are connected
-.then(() => visitor.query("select * from visitors")) // query from the table to get table
-.then((results => console.table(results.row)))
-.catch(e => console.log(err))    // catches an error
-.finally(() => visitor.end())     // is used to end the connection
+async function viewTable(){
+  await client.connect()
+  console.log("Connected successfully.")
+  const results = await client.query("select * from visitors")
+  console.table(results.rows)
+  await client.end()
+  console.log("Client disconnected successfully.")
+}
 
-module.exports = Visitors;
+async function addVisitor(id, name, age, date, time, assistedBy, comments) {
+  try{
+    await client.connect()
+    await client.query("BEGIN")
+    await client.query("insert into visitors values ($1, $2, $3, $4, $5, $6, $7)", [id, name, age, date, time, assistedBy, comments])
+    console.log("Inserted a new row")
+    await client.query("COMMIT")
+  }
+  catch(ex){
+    console.log("Failed to add visitor " + ex)
+  }
+  finally{
+    await client.end()
+    console.log("script closed")
+  }
+}
+
+async function deleteVisit() {
+  try {
+    await client.connect()
+    await client.query("BEGIN")
+    await client.query("delete from visitors where visitor_id=$1", [id])
+    console.log("visitor deleted")
+    await client.query("COMMIT")
+      
+  }
+  catch(ex){
+    console.log("Failed to delete visitor " + ex)
+  }
+  finally{
+    await client.end()
+    console.log("script closed")
+  }
+}
+async function deleteAllVisitors() {
+  try{
+    await client.connect()
+    await client.query("BEGIN")
+    await client.query("delete from visitors")
+    console.log("Deleted all visitors")
+    await client.query("COMMIT")
+  }
+  catch(ex){
+    console.log("Failed to delete visitors" + ex)
+  }
+  finally{
+    await client.end()
+    console.log("script closed")
+  }
+}
+
+addVisitor(1, 'Xolani', 25, '03-24-2020', '20:00', 'Kurtlin', 'He was drinking lean the whole time')
+addVisitor(2, 'Lebo', 18, '03-24-2020', '20:00', 'Lentswe', 'I did not like Lentswe')
+viewTable()
+// deleteAllVisitors()
+// viewTable()
